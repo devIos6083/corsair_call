4ae868c4f04bfba2c7f925e43dcfabdef18bef03
Corsair <khkyu9799@ajou.ac.kr>
2025-01-05 21:51:40 +0900

    Update agora.dart

--- corsair_call/lib/const/agora.dart
+++ corsair_call/lib/const/agora.dart
@@ -1,4 +1,4 @@
-const APP_ID = "f9f6c961f3674233b1f8b55a48b1ac95";
-const CHANNEL_NAME = "testchannel";
+const APP_ID = "YOUR_APP_ID";
+const CHANNEL_NAME = "YOUR_CHANNEL_ID";
 const TEMP_TOKEN =
-    "007eJxTYJiUzu3W/slScuslk0BlT0+D/6z7DmXYMP3JXbX05y/TD8oKDGmWaWbJlmaGacZm5iZGxsZJhmkWSaamiSYWSYaJyZamn2eWpzcEMjJsirJnYmSAQBCfm6EktbgkOSMxLy81h4EBAP+TIgY=";
+    "YOUR_TOKEN_ID";
